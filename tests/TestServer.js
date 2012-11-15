7d8bcac8830e3d851fbb57abcfdc3e68f874a4bd
Schabse Laks <Dev@SLaks.net>
2012-11-15 09:47:08 -0500

    Enable default document for directories in Styliner test server

--- tests/TestServer.js
+++ tests/TestServer.js
@@ -9,6 +9,25 @@ var server = new capsela.Server(8774)
 	.addStage(
 		function (request) {
 			var path = request.url.replace(/^\//, '');
+			var filePath = qfs.join(styliner.baseDir, path);
+
+			var self = this;
+			if (/\/$/.test(path))
+				return self.pass(request);
+
+			return qfs.isDirectory(filePath).then(function (isDir) {
+				if (isDir)
+					return new capsela.Redirect(request.getBaseUrl() + "/" + path + "/");
+				else
+					return self.pass(request);
+			});
+		})
+	.addStage(
+		function (request) {
+			var path = request.url.replace(/^\//, '');
+
+			if (/\/$/.test(path))
+				path += "index.html";
 
 			var filePath = qfs.join(styliner.baseDir, path);
 			return qfs.isFile(filePath)
@@ -16,7 +35,7 @@ var server = new capsela.Server(8774)
 						if (!exists)
 							return new capsela.Response(404);
 
-						return qfs.read(filePath)
+						return qfs.read(filePath);
 					})
 					.then(function (source) {
 						return styliner.processHTML(source);
@@ -36,7 +55,7 @@ var server = new capsela.Server(8774)
 							err.stack,
 							"text/plain"
 						);
-					})
+					});
 		});
 
 server.start();
